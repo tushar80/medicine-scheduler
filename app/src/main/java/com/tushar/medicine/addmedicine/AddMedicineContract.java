89bdc9654ceb8e461e4cc709cc2bc6b97a2e7eed
Tushar Patil <patiltushar80@yahoo.com>
2021-11-19 14:06:41 +0530

    Add AddMedicine

---
+++ app/src/main/java/com/tushar/medicine/addmedicine/AddMedicineContract.java
@@ -0,0 +1,43 @@
+package com.tushar.medicine.addmedicine;
+
+import com.tushar.medicine.BasePresenter;
+import com.tushar.medicine.BaseView;
+import com.tushar.medicine.data.source.MedicineAlarm;
+import com.tushar.medicine.data.source.Pills;
+
+import java.util.List;
+
+public interface AddMedicineContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void showEmptyMedicineError();
+
+        void showMedicineList();
+
+        boolean isActive();
+
+    }
+
+    interface  Presenter extends BasePresenter{
+
+
+        void saveMedicine(MedicineAlarm alarm, Pills pills);
+
+
+        boolean isDataMissing();
+
+        boolean isMedicineExits(String pillName);
+
+        long addPills(Pills pills);
+
+        Pills getPillsByName(String pillName);
+
+        List<MedicineAlarm> getMedicineByPillName(String pillName);
+
+        List<Long> tempIds();
+
+        void deleteMedicineAlarm(long alarmId);
+
+    }
+}
