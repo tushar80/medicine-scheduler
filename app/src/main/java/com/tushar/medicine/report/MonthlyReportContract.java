54f11418923b1c9fdfb44182aea17f60a7fb8c40
Tushar Patil <patiltushar80@yahoo.com>
2021-11-20 10:38:19 +0530

    Add report capability

---
+++ app/src/main/java/com/tushar/medicine/report/MonthlyReportContract.java
@@ -0,0 +1,44 @@
+package com.tushar.medicine.report;
+
+import com.tushar.medicine.BasePresenter;
+import com.tushar.medicine.BaseView;
+import com.tushar.medicine.data.source.History;
+import java.util.List;
+
+public interface MonthlyReportContract {
+
+    interface View extends BaseView<Presenter> {
+
+        void setLoadingIndicator(boolean active);
+
+        void showHistoryList(List<History> historyList);
+
+        void showLoadingError();
+
+        void showNoHistory();
+
+        void showTakenFilterLabel();
+
+        void showIgnoredFilterLabel();
+
+        void showAllFilterLabel();
+
+        void showNoTakenHistory();
+
+        void showNoIgnoredHistory();
+
+        boolean isActive();
+
+        void showFilteringPopUpMenu();
+
+    }
+
+    interface Presenter extends BasePresenter {
+
+        void loadHistory(boolean showLoading);
+
+        void setFiltering(FilterType filterType);
+
+        FilterType getFilterType();
+    }
+}
